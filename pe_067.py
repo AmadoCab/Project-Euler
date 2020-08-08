b2104d43c337e467563cb64196062f9acd362bf1
Macbook <Macbook@MacBook-Pro-de-Macbook.local>
2020-08-07 23:31:02 -0600

    funcionando todo

--- pe_067.py
+++ pe_067.py
@@ -1,4 +1,6 @@
+# Esta función es la encargada de hacer las comparaciones en la matriz
 def reducción_piramidal(matriz):
+    """La función recibe como parametro una matriz triangular inferior"""
     tabla = matriz
     longitud = len(tabla[-1])
     if longitud == 1:
@@ -14,14 +16,16 @@ def reducción_piramidal(matriz):
         tabla.remove(tabla[-1])
         reducción_piramidal(tabla)
 
+# Aquí almacenaremos la matriz triangular que obtendremos del documento \
+# triangulo.txt
 triangulo = []
 
+# Aquí leemos el documento y lo convertimos en listas numéricas con las que \
+# podamos trabajar
 with open('triangulo.txt', 'r') as f:
     for i in range(100):
         variable = f.readline().strip().split()
         variable = [int(n) for n in variable]
         triangulo.append(variable)
-
-#print(triangulo)
 
 reducción_piramidal(triangulo)
